476a9d1e9cf3360d74e26a4419f996f147f4f1bd
Dallibo <denismalyarenko1@gmail.com>
2023-08-05 13:53:24 +0300

    hw

---
+++ Knowledge_js_check_8.js
@@ -0,0 +1,6 @@
+let array = [
+    {name: "Bread", count: 4, bought: false},
+    {name: "Butter", count: 2, bought: true},
+    {name: "Water", count: 3, bought: false},
+    {name: "Juice", count: 1, bought: true},
+]
